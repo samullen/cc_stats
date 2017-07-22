bf7f8cd34f7c62e8752d37970aa9832694463205
Samuel Mullen <samuel@samuelmullen.com>
2017-07-22 15:35:52 -0500

    add PageView::daily_top_urls_since

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,3 +1,12 @@
 class PageView < Sequel::Model
-
+  def self.daily_top_urls_since(date)
+    sql = <<-SQL
+      SELECT created_at::date, url, count(url) as visits
+        FROM page_views 
+       WHERE created_at > '#{date}'
+       GROUP BY created_at::date, url
+       ORDER BY created_at desc
+    SQL
+    self.db[sql].all.group_by {|pv| pv[:created_at].to_s(:db)}
+  end
 end
