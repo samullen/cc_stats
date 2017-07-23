87a8f7051e431cfd8e71eb72270135bc2a6fd0b2
Samuel Mullen <samuel@samuelmullen.com>
2017-07-23 11:10:11 -0500

    build out report framework component

---
+++ app/javascript/report_framework.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import TopPageViewsReport from "./top_pageviews_report"
+import TopReferrersReport from "./top_referrers_report"
+
+class ReportFramework extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      report: "top_pageviews",
+    };
+  }
+
+  handleTabClick(report) {
+    console.log(report)
+
+    this.setState({report: report})
+  }
+
+  render() {
+    let report
+
+    if (this.state.report === "top_referrers") {
+      report = <div>top referrers report</div>
+    } else {
+      report = <div>top pageviews report</div>
+    }
+      
+    return (
+      <div>
+        <ul className="nav nav-tabs">
+          <li role="presentation" className="active">
+            <a href="#" onClick={() => this.handleTabClick("top_pageviews")}>Top PageViews</a>
+          </li>
+          <li role="presentation">
+            <a href="#" onClick={() => this.handleTabClick("top_referrers")}>Top Referrers</a>
+          </li>
+        </ul>
+
+        {report}
+      </div>
+    )
+  }
+}
+
+export default ReportFramework
