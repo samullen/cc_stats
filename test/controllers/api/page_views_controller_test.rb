8f1b5980354d331fe45c0ff41487170de2f51ac9
Samuel Mullen <samuel@samuelmullen.com>
2017-07-23 13:42:18 -0500

    ...

--- test/controllers/api/page_views_controller_test.rb
+++ test/controllers/api/page_views_controller_test.rb
@@ -7,4 +7,11 @@ class API::PageViewsControllerTest < ActionDispatch::IntegrationTest
     @controller.action_name.must_equal "daily_top_urls"
     @response.body.must_equal "{}"
   end
+
+  test "GET :: /api/page_views/daily_top_referrers" do
+    get daily_top_referrers_api_page_views_path
+
+    @controller.action_name.must_equal "daily_top_referrers"
+    @response.body.must_equal "{}"
+  end
 end
