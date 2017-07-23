82eef1748fa8125316e1f8c2fc2d2e753a9b9161
Samuel Mullen <samuel@samuelmullen.com>
2017-07-23 11:10:29 -0500

    add initial report sections

---
+++ app/javascript/top_pageviews_report.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class TopPageViewsReport extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      records: "top_pageviews",
+    };
+  }
+
+  render() {
+    const records = 
+
+    return (
+      <table className="table table-hover">
+        {records}
+      </table>
+    )
+  }
+}
+
+export default TopPageViewsReport
---
+++ app/javascript/top_referrers_report.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class TopReferrersReport extends React.Component {
+}
+
+export default TopReferrersReport
