79d61d9b84c12f0dd81a704821224b39cc1268cc
Samuel Mullen <samuel@samuelmullen.com>
2017-07-23 13:39:21 -0500

    Add caching to page_views controller

--- app/controllers/api/page_views_controller.rb
+++ app/controllers/api/page_views_controller.rb
@@ -1,12 +1,16 @@
 class API::PageViewsController < ApplicationController
   def daily_top_urls
-    results = PageView.daily_top_urls_since(5.days.ago.to_date)
+    results = Rails.cache.fetch("daily_top_urls", expires_in: 1.hour) do
+      PageView.daily_top_urls_since(5.days.ago.to_date)
+    end
 
     render json: results.to_json, status: 200
   end
 
   def daily_top_referrers
-    results = PageView.daily_top_referrers_since(5.days.ago.to_date)
+    results = Rails.cache.fetch("daily_top_referrers", expires_in: 1.hour) do
+      PageView.daily_top_referrers_since(5.days.ago.to_date)
+    end
 
     render json: results.to_json, status: 200
   end
